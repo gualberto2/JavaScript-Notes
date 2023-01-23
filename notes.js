17e96dc283370b8e0869333db3dab37a417e31ca
gualberto2 <elijahwilsonnn@gmail.com>
2023-01-23 12:40:36 -0800

    finoished Looping through Objects

--- notes.js
+++ notes.js
@@ -430,3 +430,53 @@ console.log(spaceship) // prints
 
 //Looping through Objects
 // the "for... in" iterator will iterate through each variable object
+let spaceship = {
+  crew: {
+    captain: { 
+        name: 'Lily', 
+        degree: 'Computer Engineering', 
+        cheerTeam() { console.log('You got this!') } 
+        },
+    'chief officer': { 
+        name: 'Dan', 
+        degree: 'Aerospace Engineering', 
+        agree() { console.log('I agree, captain!') } 
+        },
+    medic: { 
+        name: 'Clementine', 
+        degree: 'Physics', 
+        announce() { console.log(`Jets on!`) } },
+    translator: {
+        name: 'Shauna', 
+        degree: 'Conservation Science', 
+        powerFuel() { console.log('The tank is full!') } 
+        }
+    }
+}; 
+
+for (let crewMember in spaceship.crew) {
+  console.log(`${crewMember}: ${spaceship.crew[crewMember].name}`)
+};
+
+for (let crewMember in spaceship.crew) {
+  console.log(`${spaceship.crew[crewMember].name}: ${spaceship.crew[crewMember].degree}`)
+};
+
+// to read print read below first
+
+// so if you read the code, for the variable "spaceship" is dipping in the "crew" object
+// and in the console.log() the inside is .crew objects objects yk foo. so like it goes from crew->
+// to every object in there so it would be captain, 'chief officer', medic, and translator, which then 
+// uses the .name + .degree  properties to print out the following 
+
+// prints
+/*
+captain: Lily
+chief officer: Dan
+medic: Clementine
+translator: Shauna
+Lily: Computer Engineering
+Dan: Aerospace Engineering
+Clementine: Physics
+Shauna: Conservation Science 
+*/
