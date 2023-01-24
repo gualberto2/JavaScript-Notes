6c8d50cae16d9a8da5eaabbb1393e7540d9640b7
gualberto2 <elijahwilsonnn@gmail.com>
2023-01-24 06:32:59 -0800

    finished getter

--- notes.js
+++ notes.js
@@ -538,14 +538,14 @@ const robot = {
   _model: '1E78V2',
   _energyLevel: 100,
   get energyLevel(){
-    if (typeof this._energyLevel === 'number'){
+    if (typeof this._energyLevel === 'number'){ // we use an if with an argument to combine a string and object if true
       return `My current energy level is ${this._energyLevel}`
-    } else {
+    } else { // this else statement is if the if is false
       return "System malfunction: cannot retrieve energy level";
     }
   }
 };
 
-console.log(robot.energyLevel)
+console.log(robot.energyLevel) // prints My current energy level is 100
 
 
